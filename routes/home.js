f3d160c5bba86efe8093f9c64f04bfd12fa5c091
siva2204 <sivaraman26r@gmail.com>
2020-07-06 12:28:07 +0530

    files added

--- routes/home.js
+++ routes/home.js
@@ -10,4 +10,8 @@ router.get("/home", ensureAuthenticated, (req, res) => {
   res.render("home", { name: req.user.name });
 });
 
+router.get("/report", ensureAuthenticated, (req, res) => {
+  res.render("report", { user: req.user });
+});
+
 module.exports = router;
