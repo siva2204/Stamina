47107bf2eaf93bd4b2200a42bb16d56cb8c71a10
siva2204 <sivaraman26r@gmail.com>
2020-08-02 19:07:20 +0530

    commit changes    ...

--- stamina/js/reminder.js
+++ stamina/js/reminder.js
@@ -1,3 +1,5 @@
+var realtimer;
+var breaktimer;
 function plusrounds() {
   var rounds = document.getElementById("rounds");
   var changerounds = parseInt(rounds.innerHTML) + 1;
@@ -22,7 +24,7 @@ function plustimeon() {
     seconds = 0;
   }
 
-  if (minutes < 9 && minutes > 0) {
+  if (minutes <= 9 && minutes >= 0) {
     document.getElementById("timeon-m").innerHTML = "0" + `${minutes}`;
   } else {
     document.getElementById("timeon-m").innerHTML = minutes;
@@ -38,9 +40,9 @@ function plustimeon() {
 function minustimeon() {
   var minutes = parseInt(document.getElementById("timeon-m").innerHTML);
   var seconds = parseInt(document.getElementById("timeon-s").innerHTML);
-  if (seconds >= 0) {
+  if (seconds > 0) {
     seconds -= 1;
-    if (seconds === 0) {
+    if (seconds === -1) {
       minutes -= 1;
       seconds = 59;
     }
@@ -51,7 +53,7 @@ function minustimeon() {
       document.getElementById("timeon-m").innerHTML = minutes;
     }
 
-    if (seconds < 9 && seconds > 0) {
+    if (seconds <= 9 && seconds >= 0) {
       document.getElementById("timeon-s").innerHTML = "0" + `${seconds}`;
     } else {
       document.getElementById("timeon-s").innerHTML = seconds;
@@ -69,7 +71,7 @@ function plustimeoff() {
     seconds = 0;
   }
 
-  if (minutes < 9 && minutes > 0) {
+  if (minutes <= 9 && minutes >= 0) {
     document.getElementById("timeoff-m").innerHTML = "0" + `${minutes}`;
   } else {
     document.getElementById("timeoff-m").innerHTML = minutes;
@@ -85,9 +87,9 @@ function plustimeoff() {
 function minustimeoff() {
   var minutes = parseInt(document.getElementById("timeoff-m").innerHTML);
   var seconds = parseInt(document.getElementById("timeoff-s").innerHTML);
-  if (seconds >= 0) {
+  if (seconds > 0) {
     seconds -= 1;
-    if (seconds === 0) {
+    if (seconds === -1) {
       minutes -= 1;
       seconds = 59;
     }
@@ -98,10 +100,75 @@ function minustimeoff() {
       document.getElementById("timeoff-m").innerHTML = minutes;
     }
 
-    if (seconds < 9 && seconds > 0) {
+    if (seconds <= 9 && seconds >= 0) {
       document.getElementById("timeoff-s").innerHTML = "0" + `${seconds}`;
     } else {
       document.getElementById("timeoff-s").innerHTML = seconds;
+    }
+  }
+}
+
+function timer() {
+  const t_minutes = parseInt(document.getElementById("timeon-m").innerHTML);
+  const t_seconds = parseInt(document.getElementById("timeon-s").innerHTML);
+  const b_minutes = parseInt(document.getElementById("timeoff-m").innerHTML);
+  const b_seconds = parseInt(document.getElementById("timeoff-s").innerHTML);
+
+  var totalminutes = t_minutes;
+  var totalseconds = t_seconds;
+
+  var breakminutes = b_minutes;
+  var breakseconds = b_seconds;
+
+  var rounds = parseInt(document.getElementById("rounds").innerHTML);
+
+  console.log(totalminutes, totalseconds, breakminutes, breakseconds, rounds);
+  var breaktimer = setInterval(break_timer, 1000);
+
+  var rb = rounds;
+  var rr = rounds;
+
+  console.log("es");
+
+  //break
+  function break_timer() {
+    if (breakseconds > 0 && rb > 0) {
+      breakseconds--;
+      if (breakseconds == 0 && breakminutes > 0) {
+        breakminutes--;
+        breakseconds == 59;
+      }
+      if (breakminutes == 0 && breakseconds == 0) {
+        realtimer = setInterval(real_timer, 1000);
+        clearInterval(breaktimer);
+        rb--;
+        totalminutes = t_minutes;
+        totalseconds = t_seconds;
+
+        console.log("1");
+      }
+      document.getElementById("minutes").innerHTML = breakminutes;
+      document.getElementById("seconds").innerHTML = breakseconds;
+    }
+  }
+  //timer
+  function real_timer() {
+    if (totalseconds > 0 && rr > 0) {
+      totalseconds--;
+      if (totalseconds == 0 && totalminutes > 0) {
+        totalminutes--;
+        totalseconds == 59;
+      }
+      if (totalseconds == 0 && totalminutes == 0) {
+        breaktimer = setInterval(break_timer, 1000);
+        clearInterval(realtimer);
+        rr--;
+        breakminutes = b_minutes;
+        breakseconds = b_seconds;
+        console.log("2");
+      }
+      document.getElementById("minutes").innerHTML = totalminutes;
+      document.getElementById("seconds").innerHTML = totalseconds;
     }
   }
 }
